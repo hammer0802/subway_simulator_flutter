7cb0618c512819cdb8592b50c6663d59768a23eb
Takanori Sato <taka_sato@yumemi.co.jp>
2020-03-03 00:46:51 +0900

    footerを作った

--- lib/screens/edit_screen.dart
+++ lib/screens/edit_screen.dart
@@ -19,7 +19,40 @@ class EditScreen extends StatelessWidget {
           backgroundColor: kPrimaryColor,
         ),
         backgroundColor: Colors.white,
+        floatingActionButton: FloatingActionButton.extended(
+          backgroundColor: kSecondaryColor,
+          elevation: 4,
+          icon: const Icon(Icons.receipt),
+          label: const Text('完成'),
+          onPressed: () {},
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: BottomAppBar(
+          color: kPrimaryColor,
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            // ignore: prefer_const_literals_to_create_immutables
+            children: <Widget>[
+              bottomAppBarText(text: '合計金額 : xxx 円'),
+              bottomAppBarText(text: 'カロリー : xxx kcal'),
+            ],
+          ),
+        ),
       ),
     );
   }
+}
+
+Widget bottomAppBarText({String text}) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 24),
+    child: Text(
+      text,
+      style: const TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  );
 }
